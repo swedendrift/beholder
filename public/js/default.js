26996fed7ff5a988311a75ffd93199ea232fb7ef
swedendrift <swedendrift@gmail.com>
2017-01-12 16:49:15 -0800

    google api dev underway

--- public/js/default.js
+++ public/js/default.js
@@ -11,6 +11,30 @@ function search() {
 document.getElementById("get-position").addEventListener("click", () => {
   const thenable = search()
   thenable.then((response) => {
-    console.log(response)
+    initMap(response)
   })
-}, false);
+}, false)
+
+function initMap(geoArray) {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 3,
+    center: {lat: -33.668, lng: -117.863}
+  })
+
+  const markerLabels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+  var markers = geoArray.map((location, i) => {
+    const loc = {
+      _id: location._id,
+      geoloc: `lat: ${location.latitude}, lng: ${location.longitude}`,
+      heading: location.heading,
+      speed: location.speed
+    }
+    return new google.maps.Marker({
+      position: loc.geoloc,
+      markerLabel: markerLabels[i % markerLabels.length]
+    });
+  });
+}
+
+var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'imgs/m'})
